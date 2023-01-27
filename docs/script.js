18163d346432253a744f184039ebb6b513d55339
Miro Rava <miro.rava.commercial@gmail.com>
2023-01-27 21:12:26 +0100

    completed maps, only some links to fix

--- docs/script.js
+++ docs/script.js
@@ -62,3 +62,13 @@ const myFunction2=()=> {
   var popup = document.getElementById("myPopup2");
   popup.classList.toggle("show")
 }
+
+const myFunction3=()=> {
+  var popup = document.getElementById("myPopup3");
+  popup.classList.toggle("show")
+}
+
+const myFunction4=()=> {
+  var popup = document.getElementById("myPopup4");
+  popup.classList.toggle("show")
+}
