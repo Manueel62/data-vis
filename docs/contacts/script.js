77663f4fe596c894e7a70c35baec72e649252229
Miro Rava <miro.rava.commercial@gmail.com>
2023-01-28 15:03:58 +0100

    added pages

---
+++ docs/contacts/script.js
@@ -0,0 +1,79 @@
+/* -- Carousel Navigation -- */
+
+let activeIndex = 0;
+
+const slides = document.getElementsByTagName("article");
+
+const handleLeftClick = () => {
+  const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : slides.length - 1;
+  
+  const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`),
+        nextSlide = document.querySelector(`[data-index="${nextIndex}"]`);
+        
+  currentSlide.dataset.status = "after";
+  
+  nextSlide.dataset.status = "becoming-active-from-before";
+  
+  setTimeout(() => {
+    nextSlide.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+}
+
+const handleRightClick = () => {
+  const nextIndex = activeIndex + 1 <= slides.length - 1 ? activeIndex + 1 : 0;
+  
+  const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`),
+        nextSlide = document.querySelector(`[data-index="${nextIndex}"]`);
+  
+  currentSlide.dataset.status = "before";
+  
+  nextSlide.dataset.status = "becoming-active-from-after";
+  
+  setTimeout(() => {
+    nextSlide.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+}
+
+/* -- Mobile Nav Toggle -- */
+
+const nav = document.querySelector("nav");
+
+const handleNavToggle = () => {  
+  nav.dataset.transitionable = "true";
+  
+  nav.dataset.toggled = nav.dataset.toggled === "true" ? "false" : "true";
+}
+
+window.matchMedia("(max-width: 800px)").onchange = e => {
+  nav.dataset.transitionable = "false";
+
+  nav.dataset.toggled = "false";
+};
+
+
+const myFunction1=()=> {
+  var popup = document.getElementById("myPopup1");
+    popup.classList.toggle("show")
+}
+
+const myFunction2=()=> {
+  var popup = document.getElementById("myPopup2");
+  popup.classList.toggle("show")
+}
+
+const myFunction3=()=> {
+  var popup = document.getElementById("myPopup3");
+  popup.classList.toggle("show")
+}
+
+const myFunction4=()=> {
+  var popup = document.getElementById("myPopup4");
+  popup.classList.toggle("show")
+}
+
+const myFunction5=()=> {
+  var popup = document.getElementById("myPopup5");
+  popup.classList.toggle("show")
+}
